4d661918e08779ae5e59a8936332dd8b78dea29c
Robin Degen <robindegen@gmail.com>
2015-04-27 17:03:04 +0200

    Added platform.h for OS detection Added term_colors.h for unix shell terminal colors

---
+++ include/aeon/platform/platform.h
@@ -0,0 +1,55 @@
+#ifndef aeon_platform_platform_h__
+#define aeon_platform_platform_h__
+
+/* OS Detection mechanism based on Boost Predef (boost >= 1.55)
+ * This header defines one or more of the following macro's:
+ *
+ * AEON_PLATFORM_OS_WINDOWS
+ * AEON_PLATFORM_OS_WINDOWS_CYGWIN
+ * AEON_PLATFORM_OS_LINUX
+ * AEON_PLATFORM_OS_OSX
+ * AEON_PLATFORM_OS_FREEBSD
+ * AEON_PLATFORM_OS_NETBSD
+ * AEON_PLATFORM_OS_OPENBSD
+ * AEON_PLATFORM_OS_ANDROID
+ * AEON_PLATFORM_OS_UNIX
+ */
+
+#if (BOOST_OS_WINDOWS)
+#include <windows.h>
+#  define AEON_PLATFORM_OS_WINDOWS 1
+#endif // BOOST_OS_WINDOWS
+
+#if (BOOST_OS_CYGWIN)
+#  define AEON_PLATFORM_OS_WINDOWS_CYGWIN 1
+#endif // BOOST_OS_CYGWIN
+
+#if (BOOST_OS_LINUX)
+#  define AEON_PLATFORM_OS_LINUX 1
+#endif // BOOST_OS_LINUX
+
+#if (BOOST_OS_MACOS)
+#  define AEON_PLATFORM_OS_OSX 1
+#endif // BOOST_OS_MACOS
+
+#if (BOOST_OS_BSD_FREE)
+#  define AEON_PLATFORM_OS_FREEBSD 1
+#endif // BOOST_OS_BSD_FREE
+
+#if (BOOST_OS_BSD_NET)
+#  define AEON_PLATFORM_OS_NETBSD 1
+#endif // BOOST_OS_BSD_NET
+
+#if (BOOST_OS_BSD_OPEN)
+#  define AEON_PLATFORM_OS_OPENBSD 1
+#endif // BOOST_OS_BSD_OPEN
+
+#if (BOOST_OS_ANDROID)
+#  define AEON_PLATFORM_OS_ANDROID 1
+#endif // BOOST_OS_ANDROID
+
+#if (BOOST_OS_UNIX)
+#  define AEON_PLATFORM_OS_UNIX 1
+#endif // BOOST_OS_UNIX
+
+#endif // aeon_platform_platform_h__
