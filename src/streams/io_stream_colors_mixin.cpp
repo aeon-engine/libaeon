3e358ba52e76d49a768fe0371d97d807214059c1
Robin Degen <robindegen@gmail.com>
2015-04-28 08:24:18 +0200

    Added terminal color support for windows to io_stream_colors_mixin

--- src/streams/io_stream_colors_mixin.cpp
+++ src/streams/io_stream_colors_mixin.cpp
@@ -12,11 +12,44 @@ namespace streams
 
 void io_stream_colors_mixin::set_color(color c, weight w /*= weight::normal*/)
 {
-    reset_color();
-
 #if (AEON_PLATFORM_OS_WINDOWS)
     // TODO Implement for windows.
+    static HANDLE std_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    int intensity = 0;
+    if (w == weight::bold)
+        intensity = FOREGROUND_INTENSITY;
+
+    switch (c)
+    {
+    case color::black:
+        SetConsoleTextAttribute(std_handle, 0);
+        break;
+    case color::red:
+        SetConsoleTextAttribute(std_handle, FOREGROUND_RED || intensity);
+        break;
+    case color::green:
+        SetConsoleTextAttribute(std_handle, FOREGROUND_GREEN || intensity);
+        break;
+    case color::yellow:
+        SetConsoleTextAttribute(std_handle, FOREGROUND_RED || FOREGROUND_GREEN || intensity);
+        break;
+    case color::blue:
+        SetConsoleTextAttribute(std_handle, FOREGROUND_BLUE || intensity);
+        break;
+    case color::magenta:
+        SetConsoleTextAttribute(std_handle, FOREGROUND_RED || FOREGROUND_BLUE || intensity);
+        break;
+    case color::cyan:
+        SetConsoleTextAttribute(std_handle, FOREGROUND_GREEN || FOREGROUND_BLUE || intensity);
+        break;
+    case color::white:
+        SetConsoleTextAttribute(std_handle, FOREGROUND_RED || FOREGROUND_GREEN || FOREGROUND_BLUE || intensity);
+        break;
+    }
 #else
+    reset_color();
+
     if (w == weight::bold)
     {
         std::cout << AEON_TERM_COLOR_BOLD;
@@ -55,7 +88,8 @@ void io_stream_colors_mixin::set_color(color c, weight w /*= weight::normal*/)
 void io_stream_colors_mixin::reset_color()
 {
 #if (AEON_PLATFORM_OS_WINDOWS)
-    // TODO Implement for windows.
+    static HANDLE std_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(std_handle, 0);
 #else
     std::cout << AEON_TERM_COLOR_RESET;
 #endif
