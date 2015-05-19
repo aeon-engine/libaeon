3e439c1769d38707c13b4c3610841a0ad61f7c88
Robin Degen <robindegen@gmail.com>
2015-05-19 16:43:52 +0200

    Initial implementation of libCurl wrappers. Requires CURL_FOUND to be set by the cmake curl findscript, otherwise libaeon will be compiled without curl support.

---
+++ include/aeon/curl.h
@@ -0,0 +1,37 @@
+#ifndef aeon_curl_h__
+#define aeon_curl_h__
+
+/******************************************************************************/
+/* Standard headers                                                           */
+/******************************************************************************/
+#include <string>
+#include <set>
+#include <queue>
+#include <memory>
+#include <utility>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <sstream>
+#include <functional>
+
+/******************************************************************************/
+/* Curl headers                                                               */
+/******************************************************************************/
+#include <curl/curl.h>
+
+/******************************************************************************/
+/* Aeon headers                                                               */
+/******************************************************************************/
+
+#include <aeon/streams.h>
+
+#define AEON_CURL_DEFAULT_TIMEOUT_MS 10000
+
+#include <aeon/curl/exceptions.h>
+#include <aeon/curl/global_wrapper.h>
+#include <aeon/curl/easy_wrapper.h>
+#include <aeon/curl/url_downloader.h>
+
+#endif // aeon_curl_h__
+
---
+++ include/aeon/curl/easy_wrapper.h
@@ -0,0 +1,33 @@
+#ifndef aeon_curl_easy_wrapper_h__
+#define aeon_curl_easy_wrapper_h__
+
+namespace aeon
+{
+namespace curl
+{
+
+typedef std::function<std::size_t(void *, std::size_t)> easy_wrapper_read_event;
+class easy_wrapper
+{
+public:
+    easy_wrapper();
+    ~easy_wrapper();
+
+    void get(const std::string &url, easy_wrapper_read_event on_read,
+        long milliseconds = AEON_CURL_DEFAULT_TIMEOUT_MS);
+
+private:
+    static size_t static_read_event_(void *buffer, size_t size, size_t nmemb,
+        easy_wrapper *wrapper);
+
+    CURL *curl_;
+    global_wrapper_ptr global_wrapper_;
+    easy_wrapper_read_event read_event_;
+};
+
+typedef std::shared_ptr<easy_wrapper> easy_wrapper_ptr;
+
+} // namespace curl
+} // namespace aeon
+
+#endif // aeon_curl_easy_wrapper_h__
