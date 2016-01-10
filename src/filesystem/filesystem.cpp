0eac3394112c20fe765dc6c29543d9bdcbba3698
Robin Degen <robindegen@gmail.com>
2016-01-10 17:24:14 +0100

    Added replacement for boost filesystem (work in progress).

---
+++ src/filesystem/filesystem.cpp
@@ -0,0 +1,38 @@
+/*
+ * ROBIN DEGEN; CONFIDENTIAL
+ *
+ * 2012 - 2015 Robin Degen
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains the property of
+ * Robin Degen and its suppliers, if any. The intellectual and technical
+ * concepts contained herein are proprietary to Robin Degen and its suppliers
+ * and may be covered by U.S. and Foreign Patents, patents in process, and are
+ * protected by trade secret or copyright law. Dissemination of this
+ * information or reproduction of this material is strictly forbidden unless
+ * prior written permission is obtained from Robin Degen.
+ */
+
+#include <aeon/filesystem.h>
+#include <aeon/platform.h>
+
+#if (defined(AEON_PLATFORM_OS_OSX))
+#include <unistd.h>
+#endif
+
+namespace aeon
+{
+namespace filesystem
+{
+
+bool exists(const std::string &path)
+{
+#if (defined(AEON_PLATFORM_OS_OSX))
+    return (access(path.c_str(), F_OK) != -1);
+#else
+    throw std::runtime_error("Filesystem is not yet implemented or untested for this platform.");
+#endif
+}
+
+} // namespace filesystem
+} // namespace aeon
