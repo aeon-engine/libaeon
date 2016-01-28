34368a3e2e3611f0949d05f2220fec3e29fc5ad0
Robin Degen <robindegen@gmail.com>
2016-01-28 11:48:16 +0100

    Added testing module with temporary_file_fixture for use with unit testing

---
+++ include/aeon/testing/temporary_file_fixture.h
@@ -0,0 +1,49 @@
+/*
+ * ROBIN DEGEN; CONFIDENTIAL
+ *
+ * 2012 - 2016 Robin Degen
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains the property of
+ * Robin Degen and its suppliers, if any. The intellectual and technical
+ * concepts contained herein are proprietary to Robin Degen and its suppliers
+ * and may be covered by U.S. and Foreign Patents, patents in process, and are
+ * protected by trade secret or copyright law. Dissemination of this
+ * information or reproduction of this material is strictly forbidden unless
+ * prior written permission is obtained from Robin Degen.
+ */
+
+#pragma once
+
+namespace aeon
+{
+namespace testing
+{
+
+class temporary_file
+{
+public:
+    temporary_file();
+    explicit temporary_file(const std::string &extension);
+
+    ~temporary_file();
+
+    std::string get_temporary_file_path() const;
+
+    bool assert_temporary_file_present();
+
+    void delete_temporary_file();
+
+protected:
+    std::string path_;
+};
+
+// If gtest was included before this header, enable the test fixture code.
+#ifdef ENABLE_TEMPORARY_FILE_FIXTURE
+struct temporary_file_fixture : public temporary_file, public ::testing::Test
+{
+};
+#endif // ENABLE_TEMPORARY_FILE_FIXTURE
+
+} // namespace testing
+} // namespace aeon
---
+++ src/testing/temporary_file_fixture.cpp
@@ -0,0 +1,55 @@
+/*
+ * ROBIN DEGEN; CONFIDENTIAL
+ *
+ * 2012 - 2016 Robin Degen
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains the property of
+ * Robin Degen and its suppliers, if any. The intellectual and technical
+ * concepts contained herein are proprietary to Robin Degen and its suppliers
+ * and may be covered by U.S. and Foreign Patents, patents in process, and are
+ * protected by trade secret or copyright law. Dissemination of this
+ * information or reproduction of this material is strictly forbidden unless
+ * prior written permission is obtained from Robin Degen.
+ */
+
+#include <aeon/testing.h>
+
+namespace aeon
+{
+namespace testing
+{
+
+temporary_file::temporary_file()
+    : path_(filesystem::generate_temporary_file_path())
+{
+}
+
+temporary_file::temporary_file(const std::string &extension)
+    : path_(filesystem::generate_temporary_file_path() + "." + extension)
+{
+}
+
+temporary_file::~temporary_file()
+{
+    if (filesystem::exists(path_))
+        delete_temporary_file();
+}
+
+std::string temporary_file::get_temporary_file_path() const
+{
+    return path_;
+}
+
+bool temporary_file::assert_temporary_file_present()
+{
+    return filesystem::exists(path_);
+}
+
+void temporary_file::delete_temporary_file()
+{
+    filesystem::delete_file(path_);
+}
+
+} // namespace testing
+} // namespace aeon
