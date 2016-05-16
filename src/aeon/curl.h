8be039afd591eba485cb3f81b8039269722cc454
Robin Degen <robindegen@gmail.com>
2016-05-16 19:08:49 +0200

    Added and fixed copyright headers in sources and cmake files.

--- src/aeon/curl.h
+++ src/aeon/curl.h
@@ -1,5 +1,19 @@
-#ifndef aeon_curl_h__
-#define aeon_curl_h__
+/*
+ * ROBIN DEGEN; CONFIDENTIAL
+ *
+ * 2012 - 2016 Robin Degen
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains the property of
+ * Robin Degen and its suppliers, if any. The intellectual and technical
+ * concepts contained herein are proprietary to Robin Degen and its suppliers
+ * and may be covered by U.S. and Foreign Patents, patents in process, and are
+ * protected by trade secret or copyright law. Dissemination of this
+ * information or reproduction of this material is strictly forbidden unless
+ * prior written permission is obtained from Robin Degen.
+ */
+
+#pragma once
 
 /******************************************************************************/
 /* Standard headers                                                           */
@@ -34,5 +48,3 @@
 #include <aeon/curl/global_wrapper.h>
 #include <aeon/curl/easy_wrapper.h>
 #include <aeon/curl/url_downloader.h>
-
-#endif // aeon_curl_h__
--- src/aeon/curl/easy_wrapper.h
+++ src/aeon/curl/easy_wrapper.h
@@ -1,5 +1,19 @@
-#ifndef aeon_curl_easy_wrapper_h__
-#define aeon_curl_easy_wrapper_h__
+/*
+ * ROBIN DEGEN; CONFIDENTIAL
+ *
+ * 2012 - 2016 Robin Degen
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains the property of
+ * Robin Degen and its suppliers, if any. The intellectual and technical
+ * concepts contained herein are proprietary to Robin Degen and its suppliers
+ * and may be covered by U.S. and Foreign Patents, patents in process, and are
+ * protected by trade secret or copyright law. Dissemination of this
+ * information or reproduction of this material is strictly forbidden unless
+ * prior written permission is obtained from Robin Degen.
+ */
+
+#pragma once
 
 namespace aeon
 {
@@ -27,5 +41,3 @@ typedef std::shared_ptr<easy_wrapper> easy_wrapper_ptr;
 
 } // namespace curl
 } // namespace aeon
-
-#endif // aeon_curl_easy_wrapper_h__
