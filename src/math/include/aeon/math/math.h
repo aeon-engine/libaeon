059c780dfe3cf72b223a23cee3450575642df548
Robin Degen <robindegen@gmail.com>
2018-04-28 15:28:17 +0200

    Added math header with constants and conversions between degree and radians.

---
+++ src/math/include/aeon/math/math.h
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2012-2018 Robin Degen
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#pragma once
+
+namespace aeon::math
+{
+
+template <typename T>
+struct constants
+{
+};
+
+template <>
+struct constants<float>
+{
+    static constexpr auto pi = 3.14159265358979323846f;
+    static constexpr auto pi_2 = 1.57079632679489661923f;
+    static constexpr auto pi_4 = 0.785398163397448309616f;
+    static constexpr auto tau = pi * 2.0f;
+};
+
+template <>
+struct constants<double>
+{
+    static constexpr auto pi = 3.14159265358979323846;
+    static constexpr auto pi_2 = 1.57079632679489661923;
+    static constexpr auto pi_4 = 0.785398163397448309616;
+    static constexpr auto tau = pi * 2.0;
+};
+
+template <typename T>
+inline auto degree_to_radian(const T degree) noexcept -> T
+{
+    return degree * constants<T>::pi / static_cast<T>(180.0);
+}
+
+template <typename T>
+inline auto radian_to_degree(const T degree) noexcept -> T
+{
+    return degree * static_cast<T>(180.0) / constants<T>::pi;
+}
+
+} // namespace aeon::math
