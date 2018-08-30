3e3fea7e7af5633e08d324dc7ee51d7b8bd479ea
Robin Degen <robindegen@gmail.com>
2018-08-30 20:54:50 +0200

    Added missing const for parameters in bitflags.

--- src/common/include/aeon/common/bitflags.h
+++ src/common/include/aeon/common/bitflags.h
@@ -29,53 +29,53 @@ namespace aeon::common
 {
 
 template <typename T>
-inline static constexpr auto get_bit(T val, unsigned int bit) noexcept
+inline static constexpr auto get_bit(const T val, const unsigned int bit) noexcept
 {
     return ((val & (1 << bit)) >> bit);
 }
 
 template <typename T>
-inline static constexpr auto get_bit_range(T val, unsigned int offset, unsigned int length) noexcept
+inline static constexpr auto get_bit_range(const T val, const unsigned int offset, const unsigned int length) noexcept
 {
     return ((val & (((1 << length) - 1) << offset)) >> offset);
 }
 
 template <typename T>
-inline static constexpr void set_bit(T &val, unsigned int bit) noexcept
+inline static constexpr void set_bit(T &val, const unsigned int bit) noexcept
 {
     val |= (1 << bit);
 }
 
 template <typename T>
-inline static constexpr void clear_bit(T &val, unsigned int bit) noexcept
+inline static constexpr void clear_bit(T &val, const unsigned int bit) noexcept
 {
     val &= (val & ~(1 << bit));
 }
 
 template <typename T>
-inline static constexpr void set_bit_flag(T &val, T flag) noexcept
+inline static constexpr void set_bit_flag(T &val, const T flag) noexcept
 {
     val |= flag;
 }
 
 template <typename T>
-inline static constexpr void clear_bit_flag(T &val, T flag) noexcept
+inline static constexpr void clear_bit_flag(T &val, const T flag) noexcept
 {
     val &= (val & (~flag));
 }
 
 template <typename T>
-inline static constexpr auto check_bit_flag(T value, T flag) noexcept
+inline static constexpr auto check_bit_flag(const T value, const T flag) noexcept
 {
     return (value & flag) == flag;
 }
 
-inline static constexpr auto get_low_nibble(unsigned char value) noexcept
+inline static constexpr auto get_low_nibble(const unsigned char value) noexcept
 {
     return static_cast<unsigned char>(value & 0x0F);
 }
 
-inline static constexpr auto get_high_nibble(unsigned char value) noexcept
+inline static constexpr auto get_high_nibble(const unsigned char value) noexcept
 {
     return static_cast<unsigned char>((value & 0xF0) >> 4);
 }
