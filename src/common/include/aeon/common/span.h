08523467a811deb69c6be020de768eedc91da5b3
Robin Degen <robindegen@gmail.com>
2018-09-02 14:43:05 +0200

    Added simplified version of span from C++20.

---
+++ src/common/include/aeon/common/span.h
@@ -0,0 +1,209 @@
+/*
+ * Copyright (c) 2012-2018 Robin Degen
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#pragma once
+
+#include <aeon/common/assert.h>
+#include <type_traits>
+#include <cstddef>
+#include <iterator>
+#include <array>
+
+namespace aeon::common
+{
+
+template <typename T>
+class span
+{
+public:
+    using element_type = T;
+    using value_type = std::remove_cv_t<T>;
+    using index_type = std::ptrdiff_t;
+    using difference_type = std::ptrdiff_t;
+    using pointer = T *;
+    using reference = T &;
+    using iterator = pointer;
+    using const_iterator = const pointer;
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+    explicit constexpr span() noexcept;
+
+    explicit constexpr span(pointer ptr, const index_type count) noexcept;
+
+    explicit constexpr span(pointer first, pointer last) noexcept;
+
+    template <std::size_t N>
+    explicit constexpr span(std::array<value_type, N> &array) noexcept;
+
+    template <std::size_t N>
+    explicit constexpr span(const std::array<value_type, N> &array) noexcept;
+
+    ~span() = default;
+
+    span(const span &) noexcept = default;
+    auto operator=(const span &) noexcept -> span & = default;
+
+    span(span &&) noexcept = default;
+    auto operator=(span &&) noexcept -> span & = default;
+
+    constexpr auto data() const noexcept -> pointer;
+
+    constexpr auto size() const noexcept -> index_type;
+
+    constexpr auto empty() const noexcept -> bool;
+
+    constexpr auto operator[](const index_type i) const noexcept -> reference;
+
+    constexpr auto operator()(const index_type i) const noexcept -> reference;
+
+    constexpr auto at(const index_type i) const -> reference;
+
+    constexpr auto begin() const noexcept -> iterator;
+
+    constexpr auto end() const noexcept -> iterator;
+
+    constexpr auto front() const noexcept -> reference;
+
+    constexpr auto back() const noexcept -> reference;
+
+private:
+    pointer data_;
+    index_type size_;
+};
+
+template <typename T>
+constexpr span<T>::span() noexcept
+    : data_{nullptr}
+    , size_{0}
+{
+}
+
+template <typename T>
+constexpr span<T>::span(pointer ptr, const index_type count) noexcept
+    : data_{ptr}
+    , size_{count}
+{
+}
+
+template <typename T>
+constexpr span<T>::span(pointer first, pointer last) noexcept
+    : data_{first}
+    , size_{std::distance(first, last)}
+{
+}
+
+template <typename T>
+template <std::size_t N>
+constexpr span<T>::span(std::array<value_type, N> &array) noexcept
+    : data_{array.data()}
+    , size_{static_cast<index_type>(array.size())}
+{
+}
+
+template <typename T>
+template <std::size_t N>
+constexpr span<T>::span(const std::array<value_type, N> &array) noexcept
+    : data_{array.data()}
+    , size_{static_cast<index_type>(array.size())}
+{
+}
+
+template <typename T>
+constexpr auto span<T>::data() const noexcept -> pointer
+{
+    return data_;
+}
+
+template <typename T>
+constexpr auto span<T>::size() const noexcept -> index_type
+{
+    return size_;
+}
+
+template <typename T>
+constexpr auto span<T>::empty() const noexcept -> bool
+{
+    return size() == 0;
+}
+
+template <typename T>
+constexpr auto span<T>::operator[](const index_type i) const noexcept -> reference
+{
+    aeon_assert(i >= 0, "Out of range.");
+    aeon_assert(i < size(), "Out of range.");
+    return *(data() + i);
+}
+
+template <typename T>
+constexpr auto span<T>::operator()(const index_type i) const noexcept -> reference
+{
+    aeon_assert(i >= 0, "Out of range.");
+    aeon_assert(i < size(), "Out of range.");
+    return *(data() + i);
+}
+
+template <typename T>
+constexpr auto span<T>::at(const index_type i) const -> reference
+{
+    if (i < 0 || i > size())
+        throw std::out_of_range{"Out of range."};
+
+    return *(data() + i);
+}
+
+template <typename T>
+constexpr auto span<T>::begin() const noexcept -> iterator
+{
+    return data();
+}
+
+template <typename T>
+constexpr auto span<T>::end() const noexcept -> iterator
+{
+    return data() + size();
+}
+
+template <typename T>
+constexpr auto span<T>::front() const noexcept -> reference
+{
+    aeon_assert(!empty(), "Span empty.");
+    return *data();
+}
+
+template <typename T>
+constexpr auto span<T>::back() const noexcept -> reference
+{
+    aeon_assert(!empty(), "Span empty.");
+    return *(data() + size() - 1);
+}
+
+template <class T, size_t N>
+span(std::array<T, N> &)->span<T>;
+
+template <class T, size_t N>
+span(const std::array<T, N> &)->span<const T>;
+
+} // namespace aeon::common
---
+++ src/common/tests/test_libaeon_common/test_span.cpp
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2012-2018 Robin Degen
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <aeon/common/span.h>
+#include <gtest/gtest.h>
+#include <vector>
+
+using namespace aeon;
+
+TEST(test_span, test_span_empty)
+{
+    common::span<int> s;
+
+    EXPECT_EQ(nullptr, s.data());
+    EXPECT_EQ(0, s.size());
+}
+
+TEST(test_span, test_span_memory_buffer)
+{
+    const auto *test = "ABCDE";
+
+    common::span s{test, 5};
+
+    EXPECT_EQ(5, s.size());
+    EXPECT_EQ('A', s.front());
+    EXPECT_EQ('E', s.back());
+
+    char expected = 'A';
+    int amount = 0;
+
+    for (const auto c : s)
+    {
+        EXPECT_EQ(c, expected++);
+        ++amount;
+    }
+
+    EXPECT_EQ(amount, s.size());
+}
+
+TEST(test_span, test_span_pointers)
+{
+    const std::vector<int> test{1, 2, 3, 4, 5};
+
+    common::span s{&test.front(), &test.back()};
+
+    // According to c++20 spec this ctor must be span(firstElem, lastElem - firstElem)
+    EXPECT_EQ(4, s.size());
+    EXPECT_EQ(1, s.front());
+    EXPECT_EQ(4, s.back());
+
+    int expected = 1;
+    int amount = 0;
+
+    for (const auto c : s)
+    {
+        EXPECT_EQ(c, expected++);
+        ++amount;
+    }
+
+    EXPECT_EQ(amount, s.size());
+}
+
+TEST(test_span, test_span_std_array)
+{
+    const std::array<int, 5> test{1, 2, 3, 4, 5};
+
+    common::span s{test};
+
+    EXPECT_EQ(5, s.size());
+    EXPECT_EQ(1, s.front());
+    EXPECT_EQ(5, s.back());
+
+    int expected = 1;
+    int amount = 0;
+
+    for (const auto c : s)
+    {
+        EXPECT_EQ(c, expected++);
+        ++amount;
+    }
+
+    EXPECT_EQ(amount, s.size());
+}
+
+TEST(test_span, test_span_std_array_non_const)
+{
+    std::array<int, 5> test{1, 2, 3, 4, 5};
+
+    common::span s{test};
+
+    EXPECT_EQ(5, s.size());
+    EXPECT_EQ(1, s.front());
+    EXPECT_EQ(5, s.back());
+
+    int expected = 1;
+    int amount = 0;
+
+    for (const auto c : s)
+    {
+        EXPECT_EQ(c, expected++);
+        ++amount;
+    }
+
+    EXPECT_EQ(amount, s.size());
+}
