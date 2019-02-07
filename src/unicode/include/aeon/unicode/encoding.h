9a113467fb4eef93754a7418373ef309bc8d7598
Robin Degen <robindegen@gmail.com>
2019-02-07 20:35:58 +0100

    Add unicode conversion functions for utf8->utf32 and utf16->utf32

--- src/unicode/include/aeon/unicode/encoding.h
+++ src/unicode/include/aeon/unicode/encoding.h
@@ -10,14 +10,16 @@ namespace aeon::unicode
 namespace utf8
 {
 
-auto to_utf16(const std::string &str) -> std::wstring;
+auto to_utf16(const std::string_view &str) -> std::wstring;
+auto to_utf32(const std::string_view &str) -> std::u32string;
 
 } // namespace utf8
 
 namespace utf16
 {
 
-auto to_utf8(const std::wstring &str) -> std::string;
+auto to_utf8(const std::wstring_view &str) -> std::string;
+auto to_utf32(const std::wstring_view &str) -> std::u32string;
 
 } // namespace utf16
 
--- src/unicode/src/encoding.cpp
+++ src/unicode/src/encoding.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2012-2019 Robin Degen
 
 #include <aeon/unicode/encoding.h>
+#include <aeon/unicode/utf_string_view.h>
 #include "ucnv_wrapper.h"
 
 namespace aeon::unicode
@@ -9,10 +10,16 @@ namespace aeon::unicode
 namespace utf8
 {
 
-auto to_utf16(const std::string &str) -> std::wstring
+auto to_utf16(const std::string_view &str) -> std::wstring
 {
     internal::uconverter converter{"utf-8"};
     return converter.to_uchars(str);
+}
+
+auto to_utf32(const std::string_view &str) -> std::u32string
+{
+    utf_string_view view{str};
+    return {std::begin(view), std::end(view)};
 }
 
 } // namespace utf8
@@ -20,10 +27,16 @@ auto to_utf16(const std::string &str) -> std::wstring
 namespace utf16
 {
 
-auto to_utf8(const std::wstring &str) -> std::string
+auto to_utf8(const std::wstring_view &str) -> std::string
 {
     internal::uconverter converter{"utf-8"};
     return converter.from_uchars(str);
+}
+
+auto to_utf32(const std::wstring_view &str) -> std::u32string
+{
+    utf_string_view view{str};
+    return {std::begin(view), std::end(view)};
 }
 
 } // namespace utf16
