1f62937bc21b98ca679eb68b90867c46f12b17d1
Robin Degen <robindegen@gmail.com>
2019-02-10 14:28:21 +0100

    Add functions for escaping and unescaping utf8 strings

---
+++ src/unicode/include/aeon/unicode/stringutils.h
@@ -0,0 +1,54 @@
+// Copyright (c) 2012-2019 Robin Degen
+
+#pragma once
+
+#include <aeon/unicode/utf_string_view.h>
+#include <aeon/unicode/exception.h>
+#include <string_view>
+#include <string>
+
+namespace aeon::unicode::stringutils
+{
+
+/*!
+ * Escape a utf-8 string (ie. a newline '\n' will get translated into "\n").
+ * This function is utf-8 aware, which means that it escape properly on character codepoint boundaries.
+ */
+auto escape(const std::string_view &str) -> std::string;
+
+/*!
+ * Escape a utf-8 string (ie. a newline '\n' will get translated into "\n").
+ * This function is utf-8 aware, which means that it escape properly on character codepoint boundaries.
+ */
+auto escape(const utf_string_view<std::string_view>::iterator begin,
+            const utf_string_view<std::string_view>::iterator end) -> std::string;
+
+class unescape_exception : public std::exception
+{
+};
+
+enum class unescape_mode
+{
+    skip_unknown,
+    error_on_unknown
+};
+
+/*!
+ * Unescape a utf-8 string (ie. "abc\n123" will become an actual newline character).
+ * This function is utf-8 aware, which means that it escape properly on character codepoint boundaries.
+ *
+ * The error mode determines what to do with unknown escape characters (ie. '\u1234'): skip or throw an exception
+ */
+auto unescape(const std::string_view &str, const unescape_mode mode = unescape_mode::error_on_unknown) -> std::string;
+
+/*!
+ * Unescape a utf-8 string (ie. "abc\n123" will become an actual newline character).
+ * This function is utf-8 aware, which means that it escape properly on character codepoint boundaries.
+ *
+ * The error mode determines what to do with unknown escape characters (ie. '\u1234'): skip or throw an exception
+ */
+auto unescape(const utf_string_view<std::string_view>::iterator begin,
+              const utf_string_view<std::string_view>::iterator end,
+              const unescape_mode mode = unescape_mode::error_on_unknown) -> std::string;
+
+} // namespace aeon::unicode::stringutils
---
+++ src/unicode/src/stringutils.cpp
@@ -0,0 +1,118 @@
+// Copyright (c) 2012-2019 Robin Degen
+
+#include <aeon/unicode/stringutils.h>
+
+namespace aeon::unicode::stringutils
+{
+
+auto escape(const std::string_view &str) -> std::string
+{
+    utf_string_view view{str};
+    return escape(std::begin(view), std::end(view));
+}
+
+auto escape(const utf_string_view<std::string_view>::iterator begin,
+            const utf_string_view<std::string_view>::iterator end) -> std::string
+{
+    std::string result;
+    result.reserve(std::distance(begin, end));
+
+    for (auto itr = begin; itr != end; ++itr)
+    {
+        switch (*itr)
+        {
+            case '\b':
+                result += "\\b";
+                break;
+            case '\f':
+                result += "\\f";
+                break;
+            case '\n':
+                result += "\\n";
+                break;
+            case '\r':
+                result += "\\r";
+                break;
+            case '\t':
+                result += "\\t";
+                break;
+            case '\\':
+                result += "\\\\";
+                break;
+            case '"':
+                result += "\\\"";
+                break;
+            case '/':
+                result += "\\/";
+                break;
+            default:
+                result += itr.character_str();
+        }
+    }
+
+    return result;
+}
+
+auto unescape(const std::string_view &str, const unescape_mode mode) -> std::string
+{
+    utf_string_view view{str};
+    return unescape(std::begin(view), std::end(view), mode);
+}
+
+auto unescape(const utf_string_view<std::string_view>::iterator begin,
+              const utf_string_view<std::string_view>::iterator end, const unescape_mode mode) -> std::string
+{
+    std::string result;
+    result.reserve(std::distance(begin, end));
+
+    for (auto itr = begin; itr != end; ++itr)
+    {
+        if (*itr == '\\')
+        {
+            ++itr;
+
+            if (itr == end)
+                throw unescape_exception{};
+
+            switch (*itr)
+            {
+                case 'b':
+                    result += '\b';
+                    break;
+                case 'f':
+                    result += '\f';
+                    break;
+                case 'n':
+                    result += '\n';
+                    break;
+                case 'r':
+                    result += '\r';
+                    break;
+                case 't':
+                    result += '\t';
+                    break;
+                case '\\':
+                case '"':
+                case '/':
+                    result += static_cast<char>(*itr);
+                    break;
+                default:
+                {
+                    if (mode == unescape_mode::error_on_unknown)
+                        throw unescape_exception{};
+
+                    result += itr.character_str();
+                }
+                break;
+            }
+        }
+        else
+        {
+            result += itr.character_str();
+        }
+    }
+
+    return result;
+}
+
+} // namespace aeon::unicode::stringutils
