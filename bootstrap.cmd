8d2e109c74e733fbb886f9deeda021aabee833af
Robin Degen <robindegen@gmail.com>
2019-03-01 19:54:12 +0100

    Switch to Visual Studio 2019 Temporarily disable the mono subsystem due to a compiler bug in vs2019: https://developercommunity.visualstudio.com/content/problem/470037/c-compiler-bug-with-template-deduction.html Update cmake submodule

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -1,5 +1,5 @@
 rd /s /q vs_build
 mkdir vs_build
 cd vs_build
-cmake ../ -G "Visual Studio 15 2017 Win64" -T host=x64 -DCMAKE_INSTALL_PREFIX=install
+cmake ../ -G "Visual Studio 16 2019" -T host=x64 -A x64 -DCMAKE_INSTALL_PREFIX=install
 cd ..
