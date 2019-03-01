fb698015fdfbd67dedd877eb6c89c2f69e93d186
Robin Degen <robindegen@gmail.com>
2019-03-01 20:13:40 +0100

    Add a fourcc function to generate a compile-time fourcc (four character code) value

---
+++ src/common/include/aeon/common/fourcc.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2012-2019 Robin Degen
+
+#pragma once
+
+#include <cstdint>
+
+namespace aeon::common
+{
+
+inline constexpr auto fourcc(const char a, const char b, const char c, const char d) noexcept
+{
+    return static_cast<std::uint32_t>(d) << 24 | static_cast<std::uint32_t>(c) << 16 |
+           static_cast<std::uint32_t>(b) << 8 | static_cast<std::uint32_t>(a);
+}
+
+} // namespace aeon::common
