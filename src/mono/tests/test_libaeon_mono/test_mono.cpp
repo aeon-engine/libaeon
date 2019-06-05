cab01d5aba3e4dfe497b156a806b1538818a376a
Robin Degen <robindegen@Robins-MacBook-Pro.local>
2019-06-05 18:41:48 +0200

    Fix warning about unused return value and use auto in mono unittests

--- src/mono/tests/test_libaeon_mono/test_mono.cpp
+++ src/mono/tests/test_libaeon_mono/test_mono.cpp
@@ -13,54 +13,55 @@
 
 TEST(test_mono, test_mono_jit_load_assembly_fail)
 {
-    aeon::mono::mono_jit &jit = mono_jit_fixture::get_jit();
-    EXPECT_THROW(jit.load_assembly("some_assembly_that_doesnt_exist_12345.dll"), aeon::mono::mono_exception);
+    const auto &jit = mono_jit_fixture::get_jit();
+    EXPECT_THROW(const auto assembly = jit.load_assembly("some_assembly_that_doesnt_exist_12345.dll"); (void)assembly;
+                 , aeon::mono::mono_exception);
 }
 
 TEST(test_mono, test_mono_jit_load_assembly)
 {
-    aeon::mono::mono_jit &jit = mono_jit_fixture::get_jit();
-    EXPECT_NO_THROW(jit.load_assembly("MonoTests.dll"));
+    const auto &jit = mono_jit_fixture::get_jit();
+    EXPECT_NO_THROW(const auto assembly = jit.load_assembly("MonoTests.dll"); (void)assembly;);
 }
 
 TEST(test_mono, test_mono_jit_get_class_fail)
 {
-    aeon::mono::mono_jit &jit = mono_jit_fixture::get_jit();
-    EXPECT_NO_THROW(aeon::mono::mono_assembly assembly = jit.load_assembly("MonoTests.dll");
-                    EXPECT_THROW(aeon::mono::mono_class cls = assembly.get_class("SomeClassThatDoesntExist12345"),
+    const auto &jit = mono_jit_fixture::get_jit();
+    EXPECT_NO_THROW(const auto assembly = jit.load_assembly("MonoTests.dll");
+                    EXPECT_THROW(const auto cls = assembly.get_class("SomeClassThatDoesntExist12345"),
                                  aeon::mono::mono_exception););
 }
 
 TEST(test_mono, test_mono_jit_get_class)
 {
-    aeon::mono::mono_jit &jit = mono_jit_fixture::get_jit();
+    const auto &jit = mono_jit_fixture::get_jit();
 
-    EXPECT_NO_THROW(aeon::mono::mono_assembly assembly = jit.load_assembly("MonoTests.dll");
-                    aeon::mono::mono_class cls = assembly.get_class("ClassInstanceTest"););
+    EXPECT_NO_THROW(const auto assembly = jit.load_assembly("MonoTests.dll");
+                    const auto cls = assembly.get_class("ClassInstanceTest"););
 }
 
 TEST(test_mono, test_mono_jit_get_method)
 {
-    aeon::mono::mono_jit &jit = mono_jit_fixture::get_jit();
+    const auto &jit = mono_jit_fixture::get_jit();
 
-    EXPECT_NO_THROW(aeon::mono::mono_assembly assembly = jit.load_assembly("MonoTests.dll");
+    EXPECT_NO_THROW(const auto assembly = jit.load_assembly("MonoTests.dll");
 
-                    aeon::mono::mono_class cls = assembly.get_class("ClassInstanceTest");
+                    const auto cls = assembly.get_class("ClassInstanceTest");
 
-                    aeon::mono::mono_class_instance obj = assembly.new_class_instance(cls);
+                    const auto obj = assembly.new_class_instance(cls);
 
-                    aeon::mono::mono_method method_Method = obj.get_method("Method");
-                    aeon::mono::mono_method method_MethodWithParameter = obj.get_method("MethodWithParameter", 1);
-                    aeon::mono::mono_method method_MethodWithParameterAndReturnValue =
+                    const auto method_Method = obj.get_method("Method");
+                    const auto method_MethodWithParameter = obj.get_method("MethodWithParameter", 1);
+                    const auto method_MethodWithParameterAndReturnValue =
                         obj.get_method("MethodWithParameterAndReturnValue", 1););
 }
 
 TEST(test_mono, test_mono_call_thunk)
 {
-    auto &jit = mono_jit_fixture::get_jit();
-    auto assembly = jit.load_assembly("MonoTests.dll");
-    auto cls = assembly.get_class("ClassInstanceTest");
-    auto method_thunk = cls.get_static_function_thunk<int(int)>("FunctionWithIntParam");
+    const auto &jit = mono_jit_fixture::get_jit();
+    const auto assembly = jit.load_assembly("MonoTests.dll");
+    const auto cls = assembly.get_class("ClassInstanceTest");
+    const auto method_thunk = cls.get_static_function_thunk<int(int)>("FunctionWithIntParam");
     const auto number = 1000;
     auto result = method_thunk(number);
     ASSERT_EQ(number + 1337, result);
@@ -68,59 +69,60 @@ TEST(test_mono, test_mono_call_thunk)
 
 TEST(test_mono, test_mono_call_thunk2)
 {
-    auto &jit = mono_jit_fixture::get_jit();
-    auto assembly = jit.load_assembly("MonoTests.dll");
-    auto cls = assembly.get_class("ClassInstanceTest");
-    auto method_thunk = cls.get_static_function_thunk<void(float, int, float)>("VoidFunction");
+    const auto &jit = mono_jit_fixture::get_jit();
+    const auto assembly = jit.load_assembly("MonoTests.dll");
+    const auto cls = assembly.get_class("ClassInstanceTest");
+    const auto method_thunk = cls.get_static_function_thunk<void(float, int, float)>("VoidFunction");
     method_thunk(13.37f, 42, 9000.0f);
 }
 
 TEST(test_mono, test_mono_call_thunk3)
 {
-    auto &jit = mono_jit_fixture::get_jit();
-    auto assembly = jit.load_assembly("MonoTests.dll");
-    auto cls = assembly.get_class("ClassInstanceTest");
-    auto method_thunk = cls.get_static_function_thunk<void(std::string)>("FunctionWithStringParam");
+    const auto &jit = mono_jit_fixture::get_jit();
+    const auto assembly = jit.load_assembly("MonoTests.dll");
+    const auto cls = assembly.get_class("ClassInstanceTest");
+    const auto method_thunk = cls.get_static_function_thunk<void(std::string)>("FunctionWithStringParam");
     method_thunk("Hello!");
 }
 
 TEST(test_mono, test_mono_call_thunk4)
 {
-    auto &jit = mono_jit_fixture::get_jit();
-    auto assembly = jit.load_assembly("MonoTests.dll");
-    auto cls = assembly.get_class("ClassInstanceTest");
-    auto method_thunk = cls.get_static_function_thunk<std::string(std::string)>("StringReturnFunction");
-    auto expected_string = std::string("Hello!");
-    auto result = method_thunk(expected_string);
+    const auto &jit = mono_jit_fixture::get_jit();
+    const auto assembly = jit.load_assembly("MonoTests.dll");
+    const auto cls = assembly.get_class("ClassInstanceTest");
+    const auto method_thunk = cls.get_static_function_thunk<std::string(std::string)>("StringReturnFunction");
+    const auto expected_string = std::string("Hello!");
+    const auto result = method_thunk(expected_string);
     ASSERT_EQ(result, std::string("The string value was: " + expected_string));
 }
 
 TEST(test_mono, test_mono_call_thunk_with_exception)
 {
-    auto &jit = mono_jit_fixture::get_jit();
-    auto assembly = jit.load_assembly("MonoTests.dll");
-    auto cls = assembly.get_class("ClassInstanceTest");
-    auto method_thunk = cls.get_static_function_thunk<void()>("ExceptionFunction");
+    const auto &jit = mono_jit_fixture::get_jit();
+    const auto assembly = jit.load_assembly("MonoTests.dll");
+    const auto cls = assembly.get_class("ClassInstanceTest");
+    const auto method_thunk = cls.get_static_function_thunk<void()>("ExceptionFunction");
     ASSERT_ANY_THROW(method_thunk());
 }
 
 TEST(test_mono, test_mono_call_method)
 {
-    auto &jit = mono_jit_fixture::get_jit();
-    auto assembly = jit.load_assembly("MonoTests.dll");
-    auto cls = assembly.get_class("ClassInstanceTest");
-    auto cls_instance = assembly.new_class_instance(cls);
-    auto method_thunk = cls_instance.get_method_thunk<void()>("Method");
+    const auto &jit = mono_jit_fixture::get_jit();
+    const auto assembly = jit.load_assembly("MonoTests.dll");
+    const auto cls = assembly.get_class("ClassInstanceTest");
+    const auto cls_instance = assembly.new_class_instance(cls);
+    const auto method_thunk = cls_instance.get_method_thunk<void()>("Method");
     method_thunk();
 }
 
 TEST(test_mono, test_mono_call_method2)
 {
-    auto &jit = mono_jit_fixture::get_jit();
-    auto assembly = jit.load_assembly("MonoTests.dll");
-    auto cls = assembly.get_class("ClassInstanceTest");
-    auto cls_instance = assembly.new_class_instance(cls);
-    auto method_thunk = cls_instance.get_method_thunk<std::string(std::string)>("MethodWithParameterAndReturnValue");
-    auto result = method_thunk("test");
+    const auto &jit = mono_jit_fixture::get_jit();
+    const auto assembly = jit.load_assembly("MonoTests.dll");
+    const auto cls = assembly.get_class("ClassInstanceTest");
+    const auto cls_instance = assembly.new_class_instance(cls);
+    const auto method_thunk =
+        cls_instance.get_method_thunk<std::string(std::string)>("MethodWithParameterAndReturnValue");
+    const auto result = method_thunk("test");
     ASSERT_EQ("Return Value: test", result);
 }
