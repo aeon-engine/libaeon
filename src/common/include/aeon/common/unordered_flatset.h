ed442129e9c89ecbea8496b74301ed5594f578cb
Robin Degen <robindegen@gmail.com>
2019-08-13 19:49:41 +0200

    Add unordered_flatset which is like an std::set but uses a vector internally and linear lookup

---
+++ src/common/include/aeon/common/unordered_flatset.h
@@ -0,0 +1,176 @@
+// Distributed under the BSD 2-Clause License - Copyright 2012-2019 Robin Degen
+
+#pragma once
+
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <initializer_list>
+
+namespace aeon::common
+{
+
+template <typename key_type_t>
+class unordered_flatset final
+{
+public:
+    using key_type = key_type_t;
+    using value_type = key_type_t;
+    using set_type = std::vector<key_type>;
+    using iterator = typename set_type::iterator;
+
+    unordered_flatset() = default;
+
+    unordered_flatset(std::initializer_list<key_type> init)
+        : set_{}
+    {
+        for (auto &&val : init)
+        {
+            insert(std::move(val));
+        }
+    }
+
+    ~unordered_flatset() = default;
+
+    unordered_flatset(const unordered_flatset &) = default;
+    auto operator=(const unordered_flatset &) -> unordered_flatset & = default;
+    unordered_flatset(unordered_flatset &&) noexcept = default;
+    auto operator=(unordered_flatset &&) noexcept -> unordered_flatset & = default;
+
+    auto insert(key_type key) -> iterator
+    {
+        auto itr = find(key);
+
+        if (itr == std::end(set_))
+            return set_.insert(std::end(set_), std::move(key));
+
+        *itr = std::move(key);
+        return itr;
+    }
+
+    auto emplace(key_type &&key) -> iterator
+    {
+        auto itr = find(key);
+
+        if (itr == std::end(set_))
+            return set_.insert(std::end(set_), std::move(key));
+
+        *itr = std::move(key);
+        return itr;
+    }
+
+    [[nodiscard]] auto contains(const key_type &key) const noexcept -> bool
+    {
+        return std::find_if(std::begin(set_), std::end(set_), [key](const auto &s) { return s == key; }) !=
+               std::end(set_);
+    }
+
+    [[nodiscard]] auto find(const key_type &key) noexcept
+    {
+        return std::find_if(std::begin(set_), std::end(set_), [key](const auto &s) { return s == key; });
+    }
+
+    [[nodiscard]] auto find(const key_type &key) const noexcept
+    {
+        return std::find_if(std::begin(set_), std::end(set_), [key](const auto &s) { return s == key; });
+    }
+
+    [[nodiscard]] auto begin() noexcept
+    {
+        return std::begin(set_);
+    }
+
+    [[nodiscard]] auto end() noexcept
+    {
+        return std::end(set_);
+    }
+
+    [[nodiscard]] auto begin() const noexcept
+    {
+        return std::begin(set_);
+    }
+
+    [[nodiscard]] auto end() const noexcept
+    {
+        return std::end(set_);
+    }
+
+    auto erase(const key_type &key)
+    {
+        auto itr = find(key);
+
+        if (itr != std::end(set_))
+            return erase(itr);
+
+        return itr;
+    }
+
+    void erase_if(std::function<bool(const key_type &)> pred)
+    {
+        for (auto obj = std::begin(set_); obj != std::end(set_);)
+        {
+            if (pred(*obj))
+            {
+                obj = set_.erase(obj);
+            }
+            else
+            {
+                ++obj;
+            }
+        }
+    }
+
+    auto erase(typename set_type::iterator itr)
+    {
+        return set_.erase(itr);
+    }
+
+    [[nodiscard]] auto empty() const
+    {
+        return std::empty(set_);
+    }
+
+    void clear()
+    {
+        set_.clear();
+    }
+
+    [[nodiscard]] auto size() const noexcept
+    {
+        return std::size(set_);
+    }
+
+    void reserve(const std::size_t size)
+    {
+        set_.reserve(size);
+    }
+
+    auto operator==(const unordered_flatset<key_type> &other) const noexcept -> bool
+    {
+        if (size() != std::size(other))
+            return false;
+
+        for (const auto &val : set_)
+        {
+            const auto result = other.find(val);
+
+            if (result == std::end(other))
+                return false;
+
+            if (*result != val)
+                return false;
+        }
+
+        return true;
+    }
+
+    auto operator!=(const unordered_flatset<key_type> &other) const noexcept -> bool
+    {
+        return !(*this == other);
+    }
+
+private:
+    set_type set_;
+};
+
+} // namespace aeon::common
---
+++ src/common/tests/test_libaeon_common/test_unordered_flatset.cpp
@@ -0,0 +1,164 @@
+// Distributed under the BSD 2-Clause License - Copyright 2012-2019 Robin Degen
+
+#include <aeon/common/unordered_flatset.h>
+#include <gtest/gtest.h>
+
+using namespace aeon;
+
+TEST(test_unordered_flatset, test_unordered_flatset_unique_insert)
+{
+    common::unordered_flatset<std::string> set;
+
+    EXPECT_EQ(0u, std::size(set));
+    EXPECT_TRUE(std::empty(set));
+
+    set.insert("A");
+    EXPECT_EQ(1u, std::size(set));
+    EXPECT_FALSE(std::empty(set));
+
+    set.insert("B");
+    EXPECT_EQ(2u, std::size(set));
+    EXPECT_FALSE(std::empty(set));
+
+    set.insert("A");
+    EXPECT_EQ(2u, std::size(set));
+    EXPECT_FALSE(std::empty(set));
+
+    set.insert("B");
+    EXPECT_EQ(2u, std::size(set));
+    EXPECT_FALSE(std::empty(set));
+
+    set.insert("C");
+    EXPECT_EQ(3u, std::size(set));
+    EXPECT_FALSE(std::empty(set));
+}
+
+TEST(test_unordered_flatset, test_unordered_flatset_unique_emplace)
+{
+    common::unordered_flatset<std::string> set;
+
+    EXPECT_EQ(0u, std::size(set));
+    EXPECT_TRUE(std::empty(set));
+
+    set.emplace("A");
+    EXPECT_EQ(1u, std::size(set));
+    EXPECT_FALSE(std::empty(set));
+
+    set.emplace("B");
+    EXPECT_EQ(2u, std::size(set));
+    EXPECT_FALSE(std::empty(set));
+
+    set.emplace("A");
+    EXPECT_EQ(2u, std::size(set));
+    EXPECT_FALSE(std::empty(set));
+
+    set.emplace("B");
+    EXPECT_EQ(2u, std::size(set));
+    EXPECT_FALSE(std::empty(set));
+
+    set.emplace("C");
+    EXPECT_EQ(3u, std::size(set));
+    EXPECT_FALSE(std::empty(set));
+}
+
+TEST(test_unordered_flatset, test_unordered_flatset_unique_insert_emplace_mixed)
+{
+    common::unordered_flatset<std::string> set;
+
+    EXPECT_EQ(0u, std::size(set));
+    EXPECT_TRUE(std::empty(set));
+
+    set.emplace("A");
+    EXPECT_EQ(1u, std::size(set));
+    EXPECT_FALSE(std::empty(set));
+
+    set.emplace("B");
+    EXPECT_EQ(2u, std::size(set));
+    EXPECT_FALSE(std::empty(set));
+
+    set.insert("A");
+    EXPECT_EQ(2u, std::size(set));
+    EXPECT_FALSE(std::empty(set));
+
+    set.insert("B");
+    EXPECT_EQ(2u, std::size(set));
+    EXPECT_FALSE(std::empty(set));
+
+    set.emplace("C");
+    EXPECT_EQ(3u, std::size(set));
+    EXPECT_FALSE(std::empty(set));
+
+    set.insert("C");
+    EXPECT_EQ(3u, std::size(set));
+    EXPECT_FALSE(std::empty(set));
+}
+
+TEST(test_unordered_flatset, test_unordered_flatset_remove_by_value)
+{
+    common::unordered_flatset<std::string> set;
+    set.emplace("A");
+    set.emplace("B");
+    set.emplace("C");
+    set.emplace("D");
+
+    EXPECT_EQ(4u, std::size(set));
+
+    EXPECT_TRUE(set.contains("B"));
+    set.erase("B");
+    EXPECT_EQ(3u, std::size(set));
+    EXPECT_FALSE(set.contains("B"));
+
+    set.erase("ABCD");
+    EXPECT_EQ(3u, std::size(set));
+}
+
+TEST(test_unordered_flatset, test_unordered_flatset_remove_by_iterator)
+{
+    common::unordered_flatset<std::string> set;
+    set.emplace("A");
+    set.emplace("B");
+    set.emplace("C");
+    set.emplace("D");
+
+    EXPECT_EQ(4u, std::size(set));
+
+    EXPECT_TRUE(set.contains("A"));
+    set.erase(std::begin(set));
+    EXPECT_FALSE(set.contains("A"));
+    EXPECT_EQ(3u, std::size(set));
+
+    EXPECT_TRUE(set.contains("B"));
+    set.erase(std::begin(set));
+    EXPECT_FALSE(set.contains("B"));
+    EXPECT_EQ(2u, std::size(set));
+
+    EXPECT_TRUE(set.contains("C"));
+    set.erase(std::begin(set));
+    EXPECT_FALSE(set.contains("C"));
+    EXPECT_EQ(1u, std::size(set));
+
+    EXPECT_TRUE(set.contains("D"));
+    set.erase(std::begin(set));
+    EXPECT_FALSE(set.contains("D"));
+    EXPECT_EQ(0u, std::size(set));
+}
+
+TEST(test_unordered_flatset, test_unordered_flatset_compare)
+{
+    common::unordered_flatset<std::string> set;
+    set.emplace("A");
+    set.emplace("B");
+    set.emplace("C");
+
+    common::unordered_flatset<std::string> set2;
+    set2.emplace("A");
+    set2.emplace("B");
+    set2.emplace("C");
+
+    EXPECT_TRUE(set == set2);
+    EXPECT_FALSE(set != set2);
+
+    set.emplace("D");
+    EXPECT_FALSE(set == set2);
+    EXPECT_TRUE(set != set2);
+}
