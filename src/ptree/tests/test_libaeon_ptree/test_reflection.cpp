4f6abbab31c5f220399a8d51468a5378fa46f8c5
Robin Degen <robindegen@gmail.com>
2019-08-24 18:00:06 +0200

    Add test for supporting base classes through reflection in combination with ptree

--- src/ptree/tests/test_libaeon_ptree/test_reflection.cpp
+++ src/ptree/tests/test_libaeon_ptree/test_reflection.cpp
@@ -7,14 +7,31 @@
 
 using namespace aeon;
 
-static const ptree::property_tree test_class_pt{{{"integer_value", 3},
+static const ptree::property_tree test_class_pt{{{"base_integer_value", 42},
+                                                 {"integer_value", 3},
                                                  {"double_value", 2.0},
                                                  {"string_value", "Hello"},
                                                  {"blob_value", ptree::blob{0x10, 0x20, 0x30}}}};
 
-class test_class final : public reflection::reflection_object
+class test_class_base : public reflection::reflection_object
+{
+public:
+    test_class_base() = default;
+    ~test_class_base() override = default;
+
+    test_class_base(const test_class_base &) = default;
+    auto operator=(const test_class_base &) -> test_class_base & = default;
+
+    test_class_base(test_class_base &&) noexcept = default;
+    auto operator=(test_class_base &&) noexcept -> test_class_base & = default;
+
+    std::int64_t base_integer_value = 0;
+};
+
+class test_class final : public test_class_base
 {
     AEON_REFLECTION_BEGIN(test_class)
+    AEON_REFLECTION_FIELD(std::int64_t, base_integer_value)
     AEON_REFLECTION_FIELD(std::int64_t, integer_value)
     AEON_REFLECTION_FIELD(double, double_value)
     AEON_REFLECTION_FIELD(std::string, string_value)
@@ -23,7 +40,7 @@ class test_class final : public reflection::reflection_object
 
 public:
     test_class() = default;
-    ~test_class() = default;
+    ~test_class() override = default;
 
     test_class(const test_class &) = default;
     auto operator=(const test_class &) -> test_class & = default;
@@ -43,6 +60,7 @@ TEST(test_ptree, ptree_to_reflection_object)
 
     ASSERT_NE(nullptr, result);
 
+    EXPECT_EQ(42, result->base_integer_value);
     EXPECT_EQ(3, result->integer_value);
     EXPECT_EQ(2.0, result->double_value);
     EXPECT_EQ("Hello", result->string_value);
@@ -53,6 +71,7 @@ TEST(test_ptree, ptree_to_reflection_object)
 TEST(test_ptree, ptree_from_reflection_object)
 {
     test_class test;
+    test.base_integer_value = 42;
     test.integer_value = 3;
     test.double_value = 2.0;
     test.string_value = "Hello";
