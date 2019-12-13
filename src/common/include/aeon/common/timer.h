ff78afca964644c80fc810cf811c1ef31185f517
Robin Degen <robindegen@gmail.com>
2019-12-13 16:07:15 +0100

    Rewrite timer class to use chrono::high_resolution_clock on all platforms Add optional template argument to get the time difference in a different type

--- src/common/include/aeon/common/timer.h
+++ src/common/include/aeon/common/timer.h
@@ -3,14 +3,7 @@
 #pragma once
 
 #include <aeon/common/platform.h>
-
-#if (defined(AEON_PLATFORM_OS_WINDOWS))
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
-#include <stdexcept>
-#else
 #include <chrono>
-#endif
 
 namespace aeon::common
 {
@@ -18,11 +11,10 @@ namespace aeon::common
 class timer
 {
 public:
-    timer()
+    using clock_type = std::chrono::high_resolution_clock;
+
+    timer() noexcept
     {
-#if (defined(AEON_PLATFORM_OS_WINDOWS))
-        QueryPerformanceFrequency(&frequency_);
-#endif
         reset();
     }
 
@@ -36,33 +28,19 @@ public:
 
     void reset() noexcept
     {
-#if (defined(AEON_PLATFORM_OS_WINDOWS))
-        QueryPerformanceCounter(&start_time_);
-#else
-        start_time_ = std::chrono::system_clock::now();
-#endif
+        start_time_ = clock_type::now();
     }
 
-    [[nodiscard]] auto get_time_difference() const noexcept -> double
+    template <typename T = float>
+    [[nodiscard]] auto get_time_difference() const noexcept
     {
-#if (defined(AEON_PLATFORM_OS_WINDOWS))
-        LARGE_INTEGER now;
-        QueryPerformanceCounter(&now);
-        return static_cast<float>(now.QuadPart - start_time_.QuadPart) / frequency_.QuadPart;
-#else
-        const auto now = std::chrono::system_clock::now();
-        std::chrono::duration<double> elapsed_seconds = now - start_time_;
+        const auto now = clock_type::now();
+        const std::chrono::duration<T> elapsed_seconds = now - start_time_;
         return elapsed_seconds.count();
-#endif
     }
 
 private:
-#if (defined(AEON_PLATFORM_OS_WINDOWS))
-    LARGE_INTEGER frequency_;
-    LARGE_INTEGER start_time_;
-#else
-    std::chrono::time_point<std::chrono::system_clock> start_time_;
-#endif
+    std::chrono::time_point<clock_type> start_time_;
 };
 
 } // namespace aeon::common
