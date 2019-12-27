656dcac2afb67e66d31957fc6d25ed232569d2a9
Robin Degen <robindegen@gmail.com>
2019-12-27 17:12:04 +0100

    Add fourcc overload from a string instead of 4 individual characters

--- src/common/include/aeon/common/fourcc.h
+++ src/common/include/aeon/common/fourcc.h
@@ -13,4 +13,10 @@ inline constexpr auto fourcc(const char a, const char b, const char c, const cha
            static_cast<std::uint32_t>(b) << 8 | static_cast<std::uint32_t>(a);
 }
 
+inline constexpr auto fourcc(const char str[5]) noexcept
+{
+    return static_cast<std::uint32_t>(str[3]) << 24 | static_cast<std::uint32_t>(str[2]) << 16 |
+           static_cast<std::uint32_t>(str[1]) << 8 | static_cast<std::uint32_t>(str[0]);
+}
+
 } // namespace aeon::common
