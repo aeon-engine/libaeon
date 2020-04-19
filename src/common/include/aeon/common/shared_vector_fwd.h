6f723492b628bb51d01af9e2aa2b39f05a88f5ef
Robin Degen <robindegen@gmail.com>
2020-04-19 20:37:59 +0200

    Add forwarding header for shared_vector

---
+++ src/common/include/aeon/common/shared_vector_fwd.h
@@ -0,0 +1,11 @@
+// Distributed under the BSD 2-Clause License - Copyright 2019-2020 Robin Degen
+
+#pragma once
+
+namespace aeon::common
+{
+
+template <typename T>
+class shared_vector;
+
+} // namespace aeon::common
