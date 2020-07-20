8726b91d56c62d0733211d79e13ed2289ce0e161
Robin Degen <robindegen@gmail.com>
2020-07-20 17:36:17 +0200

    Add forward header for imaging::dynamic_image

---
+++ src/imaging/include/aeon/imaging/dynamic_image_fwd.h
@@ -0,0 +1,10 @@
+// Distributed under the BSD 2-Clause License - Copyright 2012-2020 Robin Degen
+
+#pragma once
+
+namespace aeon::imaging
+{
+
+class dynamic_image;
+
+} // namespace aeon::imaging
