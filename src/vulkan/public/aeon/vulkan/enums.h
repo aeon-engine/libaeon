af45ec9ba081e45e8518c81987ea997aa2b30ea7
Robin Degen <robindegen@gmail.com>
2021-03-28 17:43:02 +0200

    Fix codefactor issue: Redundant blank line at the end of a code block should be deleted

--- src/vulkan/public/aeon/vulkan/enums.h
+++ src/vulkan/public/aeon/vulkan/enums.h
@@ -12,7 +12,6 @@ enum class attachment_load_op
     load = VK_ATTACHMENT_LOAD_OP_LOAD,
     clear = VK_ATTACHMENT_LOAD_OP_CLEAR,
     dont_care = VK_ATTACHMENT_LOAD_OP_DONT_CARE
-
 };
 
 enum class attachment_store_op
