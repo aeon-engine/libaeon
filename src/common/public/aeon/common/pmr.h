c67c89d0dbc0659200d12840d19cd2bafb8962b4
Robin Degen <robindegen@gmail.com>
2021-05-03 16:03:55 +0200

    Replace the usage of std::function in pmr::unique_ptr with a custom deleter object which reduces the unique_ptr size from 72 bytes down to 16 bytes on Visual Studio

--- src/common/public/aeon/common/pmr.h
+++ src/common/public/aeon/common/pmr.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include <aeon/common/compilers.h>
 #include <memory_resource>
 #include <functional>
 #include <memory>
@@ -9,15 +10,30 @@
 namespace aeon::common::pmr
 {
 
+namespace internal
+{
+
 template <typename T>
-using unique_ptr = std::unique_ptr<T, std::function<void(T *)>>;
+struct unique_ptr_pmr_deleter
+{
+    AEON_FORCE_INLINE void operator()(T *ptr)
+    {
+        allocator->deallocate(ptr, sizeof(T), alignof(T));
+    }
+
+    std::pmr::memory_resource *allocator;
+};
+
+} // namespace internal
+
+template <typename T>
+using unique_ptr = std::unique_ptr<T, internal::unique_ptr_pmr_deleter<T>>;
 
 template <typename T, typename... args_t>
 [[nodiscard]] auto make_unique(std::pmr::memory_resource *allocator, args_t &&...args)
 {
-    return unique_ptr<T>(
-        new (allocator->allocate(sizeof(T), alignof(T))) T(std::forward<args_t>(args)...),
-        [allocator, size = sizeof(T), align = alignof(T)](T *ptr) { allocator->deallocate(ptr, size, align); });
+    return unique_ptr<T>(new (allocator->allocate(sizeof(T), alignof(T))) T(std::forward<args_t>(args)...),
+                         internal::unique_ptr_pmr_deleter<T>{allocator});
 }
 
 } // namespace aeon::common::pmr
