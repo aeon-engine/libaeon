44387af48ea1d39063be4381c0e3f0549794deb0
Robin Degen <robindegen@gmail.com>
2021-07-20 21:48:03 +0200

    Add string traits to determine the most appropriate basic_string and basic_string_view types for a given string-like and to determine the used character type

--- src/common/public/aeon/common/string_traits.h
+++ src/common/public/aeon/common/string_traits.h
@@ -68,4 +68,142 @@ struct convert<bool>
     }
 };
 
+template <typename T>
+struct string_type_traits
+{
+};
+
+template <typename T>
+struct string_type_traits<std::basic_string<T>>
+{
+    using basic_string_type = std::basic_string<T>;
+    using basic_string_view_type = std::basic_string_view<T>;
+    using char_type = T;
+};
+
+template <typename T>
+struct string_type_traits<std::basic_string_view<T>>
+{
+    using basic_string_type = std::basic_string<T>;
+    using basic_string_view_type = std::basic_string_view<T>;
+    using char_type = T;
+};
+
+template <>
+struct string_type_traits<char>
+{
+    using basic_string_type = std::basic_string<char>;
+    using basic_string_view_type = std::basic_string_view<char>;
+    using char_type = char;
+};
+
+template <>
+struct string_type_traits<wchar_t>
+{
+    using basic_string_type = std::basic_string<wchar_t>;
+    using basic_string_view_type = std::basic_string_view<wchar_t>;
+    using char_type = wchar_t;
+};
+
+template <>
+struct string_type_traits<char8_t>
+{
+    using basic_string_type = std::basic_string<char8_t>;
+    using basic_string_view_type = std::basic_string_view<char8_t>;
+    using char_type = char8_t;
+};
+
+template <>
+struct string_type_traits<char16_t>
+{
+    using basic_string_type = std::basic_string<char16_t>;
+    using basic_string_view_type = std::basic_string_view<char16_t>;
+    using char_type = char16_t;
+};
+
+template <>
+struct string_type_traits<char32_t>
+{
+    using basic_string_type = std::basic_string<char32_t>;
+    using basic_string_view_type = std::basic_string_view<char32_t>;
+    using char_type = char32_t;
+};
+
+template <int size>
+struct string_type_traits<char[size]>
+{
+    using basic_string_type = std::basic_string<char>;
+    using basic_string_view_type = std::basic_string_view<char>;
+    using char_type = char;
+};
+
+template <int size>
+struct string_type_traits<wchar_t[size]>
+{
+    using basic_string_type = std::basic_string<wchar_t>;
+    using basic_string_view_type = std::basic_string_view<wchar_t>;
+    using char_type = wchar_t;
+};
+
+template <int size>
+struct string_type_traits<char8_t[size]>
+{
+    using basic_string_type = std::basic_string<char8_t>;
+    using basic_string_view_type = std::basic_string_view<char8_t>;
+    using char_type = char8_t;
+};
+
+template <int size>
+struct string_type_traits<char16_t[size]>
+{
+    using basic_string_type = std::basic_string<char16_t>;
+    using basic_string_view_type = std::basic_string_view<char16_t>;
+    using char_type = char16_t;
+};
+
+template <int size>
+struct string_type_traits<char32_t[size]>
+{
+    using basic_string_type = std::basic_string<char32_t>;
+    using basic_string_view_type = std::basic_string_view<char32_t>;
+    using char_type = char32_t;
+};
+
+/*!
+ * Determine the most appropriate std::basic_string type for a given string-like.
+ * For example:
+ * const char * -> std::string
+ * const wchar_t * -> std::wstring
+ * char[5] -> std::string
+ * std::string -> std::string
+ * std::string_view -> std::string
+ */
+template <typename T>
+using basic_string_type = typename string_type_traits<std::remove_pointer_t<std::decay_t<T>>>::basic_string_type;
+
+/*!
+ * Determine the most appropriate std::basic_string_view type for a given string-like.
+ * For example:
+ * const char * -> std::string_view
+ * const wchar_t * -> std::wstring_view
+ * char[5] -> std::string_view
+ * std::string -> std::string_view
+ * std::string_view -> std::string_view
+ */
+template <typename T>
+using basic_string_view_type =
+    typename string_type_traits<std::remove_pointer_t<std::decay_t<T>>>::basic_string_view_type;
+
+/*!
+ * Determine the character type for a given string-like.
+ * For example:
+ * const char * -> char
+ * const wchar_t * -> wchar_t
+ * char[5] -> char
+ * std::string -> char
+ * std::string_view -> char
+ */
+template <typename T>
+using char_type = typename string_type_traits<std::remove_pointer_t<std::decay_t<T>>>::char_type;
+
 } // namespace aeon::common::string
