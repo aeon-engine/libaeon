e38b0c6f9a826ba837faad68d8ad27f3878a68fc
Robin Degen <robindegen@gmail.com>
2021-07-24 00:16:12 +0200

    Add u8_stream header to common to allow for printing utf-8 std::u8string_view to std::cout. This will assume that the terminal supports utf-8

---
+++ src/common/public/aeon/common/u8_stream.h
@@ -0,0 +1,16 @@
+// Distributed under the BSD 2-Clause License - Copyright 2012-2021 Robin Degen
+
+#pragma once
+
+#include <ostream>
+#include <string>
+
+/*!
+ * Write an u8string to an ostream. This assumes that the output stream (for example the terminal, in case of std::out)
+ * supports UTF-8. No conversion is done.
+ */
+inline auto operator<<(std::ostream &os, const std::u8string_view &s) -> std::ostream &
+{
+    // This is not perfectly portable, but works fine on all major platforms
+    return os << std::string_view{reinterpret_cast<const char *>(std::data(s)), std::size(s)};
+}
---
+++ src/common/tests/test_libaeon_common/test_u8stream.cpp
@@ -0,0 +1,19 @@
+﻿// Distributed under the BSD 2-Clause License - Copyright 2012-2021 Robin Degen
+
+#include <aeon/common/u8_stream.h>
+#include <gtest/gtest.h>
+#include <iostream>
+#include <sstream>
+
+TEST(test_u8stream, test_u8stream_stringstream)
+{
+    std::stringstream ss;
+    ss << std::u8string_view{u8"This is a string"};
+    EXPECT_EQ(ss.str(), "This is a string");
+}
+
+TEST(test_u8stream, test_u8stream_iostream)
+{
+    std::cout << std::u8string_view{u8"This is a string.\n"};
+    std::cout << std::u8string_view{u8"らき☆すた, Raki☆Suta.\n"};
+}
