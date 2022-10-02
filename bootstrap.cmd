444916c233eb092099dc67f167a297f43d84297f
Robin Degen <robindegen@gmail.com>
2022-10-02 15:57:58 +0200

    Run conan install from CMake instead of the bootstrap scripts for better IDE integration

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -1,5 +1,2 @@
 rd /s /q build
-mkdir build
-conan install . -if build -s build_type=Debug
-conan install . -if build -s build_type=Release
 cmake  . -B build -G "Visual Studio 17 2022" -T host=x64 -A x64 -DCMAKE_SYSTEM_VERSION=10.0 -DCMAKE_INSTALL_PREFIX=%CD%/build/install
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 rm -rf build
-mkdir build
-conan install . -if build -s build_type=Debug
-cmake . -B build -DCMAKE_INSTALL_PREFIX=$(pwd)/build/install -DCMAKE_BUILD_TYPE=Debug
+cmake . -B build -DCMAKE_INSTALL_PREFIX=$(pwd)/build/install
